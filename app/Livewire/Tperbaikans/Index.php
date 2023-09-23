7d35a37f5f3aaad3fcffc929ab9f1ff8a5db2320
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-09-23 07:17:05 +0700

    .

--- app/Livewire/Tperbaikans/Index.php
+++ app/Livewire/Tperbaikans/Index.php
@@ -8,6 +8,9 @@ use Livewire\WithPagination;
 
 class Index extends Component
 {
+
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.tperbaikans.index', [
